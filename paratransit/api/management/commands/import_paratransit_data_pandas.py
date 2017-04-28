21a1f778d17d1b47908896c7e06e27ac1e3da3f5
JD Godchaux <jd@nijel.org>
2017-04-28 17:46:45 -0400

    updating pandas code

--- paratransit/api/management/commands/import_paratransit_data_pandas.py
+++ paratransit/api/management/commands/import_paratransit_data_pandas.py
@@ -2,10 +2,26 @@ import sys,os
 from django.core.management.base import BaseCommand, CommandError
 from api.models import *
 import csv
-import pandas
-import datetime
 from django.db import connection
 from django.conf import settings
+
+import pandas as pd
+from sqlalchemy import create_engine
+import datetime as dt
+
+#db connection
+user = settings.DATABASES['default']['USER']
+password = settings.DATABASES['default']['PASSWORD']
+database_name = settings.DATABASES['default']['NAME']
+
+database_url = 'postgresql://{user}:{password}@localhost:5432/{database_name}'.format(
+    user=user,
+    password=password,
+    database_name=database_name,
+)
+
+engine = create_engine(database_url, echo=False)
+
 
 
 """
@@ -32,23 +48,33 @@ class Command(BaseCommand):
 		cursor.execute("TRUNCATE TABLE api_trips RESTART IDENTITY;")
 	
 	def load_paratransit_data(self):
+		# load variables
+		start = datetime.datetime.now()
+		chunksize = 20000
+		j = 0
+		index_start = 1
+
 		# location of file
 		filename = os.path.join('/mnt/volume-nyc1-01/paratransit_raw_data/', 'paratransit_2015_vars.csv')
 
-		df = pandas.read_csv(filename)
+		for df in pd.read_csv(filename, chunksize=chunksize, iterator=True, encoding='utf-8'):
 
-		user = settings.DATABASES['default']['USER']
-		password = settings.DATABASES['default']['PASSWORD']
-		database_name = settings.DATABASES['default']['NAME']
+			df = df.rename(columns={c: c.replace(' ', '') for c in df.columns}) # Remove spaces from columns
 
-		database_url = 'postgresql://{user}:{password}@localhost:5432/{database_name}'.format(
-		    user=user,
-		    password=password,
-		    database_name=database_name,
-		)
+			df['tripdate'] = pd.to_datetime(df['tripdate'])
+			df['picktime'] = pd.to_datetime(df['picktime'])
+			df['droptime'] = pd.to_datetime(df['droptime'])
+			df['pickdate'] = pd.to_datetime(df['pickdate'])
+			df['dropdate'] = pd.to_datetime(df['dropdate'])
 
-		engine = create_engine(database_url, echo=False)
-		df.to_sql(trips, con=engine)
+			df.index += index_start
+
+			j+=1
+			print '{} seconds: completed {} rows'.format((dt.datetime.now() - start).seconds, j*chunksize)
+			
+			df.to_sql(trips, con=engine)
+			index_start = df.index[-1] + 1
+
 
 	def handle(self, *args, **options):
 		print "Truncate Table...."
